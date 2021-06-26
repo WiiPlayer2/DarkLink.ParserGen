1dbf9d90a4522100411c21ecf7c44d4add9bc12b
Waldemar Tomme <waldetomme@gmail.com>
2021-06-26 13:24:56 +0200

    Split up Generator.cs

---
+++ DarkLink.ParserGen/Generator.Token.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DarkLink.ParserGen
+{
+    partial class Generator
+    {
+        private void GenerateToken(TextWriter writer)
+        {
+            writer.WriteLine($@"
+        public record Token(TokenType Type, string Value, int Index);");
+        }
+    }
+}
---
+++ DarkLink.ParserGen/Generator.TokenType.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DarkLink.ParserGen
+{
+    partial class Generator
+    {
+        private void GenerateTokenType(TextWriter writer, Config config)
+        {
+            writer.WriteLine($@"
+        public enum TokenType
+        {{");
+
+            foreach (var tokenInfo in config.Lexer.Tokens)
+                writer.WriteLine($@"
+            {tokenInfo.Name},");
+
+            writer.WriteLine($@"
+            UNDEFINED,
+        }}
+");
+        }
+    }
+}
