9992e05aca524d5869cdb5f3b34d21613737ca02
Waldemar Tomme <waldetomme@gmail.com>
2021-06-26 11:35:25 +0200

    Use a single static class

--- DarkLink.ParserGen/Generator.cs
+++ DarkLink.ParserGen/Generator.cs
@@ -38,15 +38,7 @@ namespace DarkLink.ParserGen
         }
 
         private void Generate(GeneratorExecutionContext context, Config config)
-        {
-            var baseName = $"{config.Type.Namespace}.{config.Type.Name}";
-            GenerateTokenType(context, baseName, config);
-            GenerateToken(context, baseName, config);
-            GenerateLexer(context, baseName, config);
-        }
-
-        private void GenerateLexer(GeneratorExecutionContext context, string baseName, Config config)
-            => AddSource(context, $"{baseName}Lexer.g.cs", writer =>
+            => AddSource(context, $"{config.Type.Namespace}.{config.Type.Name}.g.cs", writer =>
             {
                 writer.WriteLine($@"
 #nullable enable
@@ -59,128 +51,144 @@ using System.Linq;
 
 namespace {config.Type.Namespace}
 {{
-    {config.Type.Modifier} class {config.Type.Name}Lexer : IEnumerator<{config.Type.Name}Token>
-    {{
-        private readonly TextReader reader;
+    {config.Type.Modifier} static class {config.Type.Name}
+    {{");
 
-        private static readonly Dictionary<{config.Type.Name}TokenType, Regex> tokenRules = new();
+                GenerateTokenType(writer, config);
+                GenerateToken(writer);
+                GenerateLexer(writer, config);
 
-        private string? input;
-
-        private int currentIndex;
-
-        static {config.Type.Name}Lexer()
-        {{");
-                foreach (var token in config.Lexer.Tokens)
-                {
-                    writer.WriteLine($"tokenRules.Add({config.Type.Name}TokenType.{token.Name}, new Regex({Microsoft.CodeAnalysis.CSharp.SymbolDisplay.FormatLiteral(token.Regex, true)}));");
-                }
                 writer.WriteLine($@"
-        }}
-
-        public {config.Type.Name}Lexer(string input)
-            : this(new StringReader(input)) {{ }}
-
-        public {config.Type.Name}Lexer(Stream stream)
-            : this(new StreamReader(stream)) {{ }}
-
-        public {config.Type.Name}Lexer(TextReader reader)
-        {{
-            this.reader = reader;
-            Current = new({config.Type.Name}TokenType.UNKNOWN, string.Empty, -1);
-        }}
-
-        public bool MoveNext()
-        {{
-            input ??= reader.ReadToEnd();
-            if (currentIndex == input.Length)
-                return false;
-
-            var (type, match) = tokenRules
-                .Select(kv => (Type: kv.Key, Match: kv.Value.Match(input, currentIndex)))
-                .Where(tuple => tuple.Match.Success && tuple.Match.Length > 0)
-                .OrderBy(tuple => tuple.Match.Index)
-                .FirstOrDefault();
-
-            if (match is null)
-            {{
-                Current = new({config.Type.Name}TokenType.UNKNOWN, input.Substring(currentIndex), currentIndex);
-                currentIndex = input.Length;
-                return true;
-            }}
-
-            if (match.Index > currentIndex)
-            {{
-                Current = new({config.Type.Name}TokenType.UNKNOWN, input.Substring(currentIndex, match.Index - currentIndex), currentIndex);
-                currentIndex = match.Index;
-                return true;
-            }}
-
-            Current = new(type, match.Value, currentIndex);
-            currentIndex += match.Length;
-            return true;
-        }}
-
-        public {config.Type.Name}Token Current {{ get; private set; }}
-
-        object IEnumerator.Current => Current;
-
-        public void Reset()
-        {{
-            currentIndex = 0;
-        }}
-
-        public void Dispose()
-        {{
-            input = null;
-            reader.Dispose();
-        }}
-
-        public static IEnumerable<{config.Type.Name}Token> Lex(string input)
-            => Lex(new StringReader(input));
-
-        public static IEnumerable<{config.Type.Name}Token> Lex(Stream stream)
-            => Lex(new StreamReader(stream));
-
-        public static IEnumerable<{config.Type.Name}Token> Lex(TextReader reader)
-            => new Enumerable(() => new {config.Type.Name}Lexer(reader));
-
-        private class Enumerable : IEnumerable<{config.Type.Name}Token>
-        {{
-            private readonly Func<IEnumerator<{config.Type.Name}Token>> enumeratorFunc;
-
-            public Enumerable(Func<IEnumerator<{config.Type.Name}Token>> enumeratorFunc)
-            {{
-                this.enumeratorFunc = enumeratorFunc;
-            }}
-
-            public IEnumerator<{config.Type.Name}Token> GetEnumerator() => enumeratorFunc();
-
-            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-        }}
     }}
 }}
 ");
             });
 
-        private void GenerateToken(GeneratorExecutionContext context, string baseName, Config config)
-            => AddSource(context, $"{baseName}Token.g.cs", writer =>
-            {
-                writer.WriteLine("#nullable enable");
-                writer.WriteLine($"namespace {config.Type.Namespace} {{");
-                writer.WriteLine($"{config.Type.Modifier} record {config.Type.Name}Token({config.Type.Name}TokenType Type, string Value, int Index);");
-                writer.WriteLine("}");
-            });
+        private void GenerateLexer(TextWriter writer, Config config)
+        {
+            writer.WriteLine($@"
+        public class Lexer : IEnumerator<Token>
+        {{
+            private readonly TextReader reader;
 
-        private void GenerateTokenType(GeneratorExecutionContext context, string baseName, Config config)
-            => AddSource(context, $"{baseName}TokenType.g.cs", writer =>
+            private static readonly Dictionary<TokenType, Regex> tokenRules = new();
+
+            private string? input;
+
+            private int currentIndex;
+
+            static Lexer()
+            {{");
+            foreach (var token in config.Lexer.Tokens)
             {
-                writer.WriteLine($"namespace {config.Type.Namespace} {{");
-                writer.WriteLine($"{config.Type.Modifier} enum {config.Type.Name}TokenType {{");
-                writer.WriteLine("UNKNOWN,");
-                foreach (var tokenInfo in config.Lexer.Tokens)
-                    writer.WriteLine($"{tokenInfo.Name},");
-                writer.WriteLine("}}");
-            });
+                writer.WriteLine($"tokenRules.Add(TokenType.{token.Name}, new Regex({Microsoft.CodeAnalysis.CSharp.SymbolDisplay.FormatLiteral(token.Regex, true)}));");
+            }
+            writer.WriteLine($@"
+            }}
+
+            public Lexer(string input)
+                : this(new StringReader(input)) {{ }}
+
+            public Lexer(Stream stream)
+                : this(new StreamReader(stream)) {{ }}
+
+            public Lexer(TextReader reader)
+            {{
+                this.reader = reader;
+                Current = new(TokenType.UNDEFINED, string.Empty, -1);
+            }}
+
+            public bool MoveNext()
+            {{
+                input ??= reader.ReadToEnd();
+                if (currentIndex == input.Length)
+                    return false;
+
+                var (type, match) = tokenRules
+                    .Select(kv => (Type: kv.Key, Match: kv.Value.Match(input, currentIndex)))
+                    .Where(tuple => tuple.Match.Success && tuple.Match.Length > 0)
+                    .OrderBy(tuple => tuple.Match.Index)
+                    .FirstOrDefault();
+
+                if (match is null)
+                {{
+                    Current = new(TokenType.UNDEFINED, input.Substring(currentIndex), currentIndex);
+                    currentIndex = input.Length;
+                    return true;
+                }}
+
+                if (match.Index > currentIndex)
+                {{
+                    Current = new(TokenType.UNDEFINED, input.Substring(currentIndex, match.Index - currentIndex), currentIndex);
+                    currentIndex = match.Index;
+                    return true;
+                }}
+
+                Current = new(type, match.Value, currentIndex);
+                currentIndex += match.Length;
+                return true;
+            }}
+
+            public Token Current {{ get; private set; }}
+
+            object IEnumerator.Current => Current;
+
+            public void Reset()
+            {{
+                currentIndex = 0;
+            }}
+
+            public void Dispose()
+            {{
+                input = null;
+                reader.Dispose();
+            }}
+
+            public static IEnumerable<Token> Lex(string input)
+                => Lex(new StringReader(input));
+
+            public static IEnumerable<Token> Lex(Stream stream)
+                => Lex(new StreamReader(stream));
+
+            public static IEnumerable<Token> Lex(TextReader reader)
+                => new Enumerable(() => new Lexer(reader));
+
+            private class Enumerable : IEnumerable<Token>
+            {{
+                private readonly Func<IEnumerator<Token>> enumeratorFunc;
+
+                public Enumerable(Func<IEnumerator<Token>> enumeratorFunc)
+                {{
+                    this.enumeratorFunc = enumeratorFunc;
+                }}
+
+                public IEnumerator<Token> GetEnumerator() => enumeratorFunc();
+
+                IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+            }}
+        }}");
+        }
+
+        private void GenerateToken(TextWriter writer)
+        {
+            writer.WriteLine($@"
+        public record Token(TokenType Type, string Value, int Index);");
+        }
+
+        private void GenerateTokenType(TextWriter writer, Config config)
+        {
+            writer.WriteLine($@"
+        public enum TokenType
+        {{");
+
+            foreach (var tokenInfo in config.Lexer.Tokens)
+                writer.WriteLine($@"
+            {tokenInfo.Name},");
+
+            writer.WriteLine($@"
+            UNDEFINED,
+        }}
+");
+        }
     }
 }
