feda9389acff25c03ecfb8a9b8412f52a6598382
Waldemar Tomme <waldetomme@gmail.com>
2025-10-04 01:05:29 +0200

    Don’t mess with Voodoo

--- DemoApp/Program.cs
+++ DemoApp/Program.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 using DarkLink.ParserGen.Parsing;
-using DemoApp.Sub;
 
 namespace DemoApp
 {
@@ -12,16 +11,9 @@ namespace DemoApp
     {
         private static void Main(string[] args)
         {
-            //var content = @"aabbaa";
-            //var parser = new Testing.Parser<S>(new()
-            //{
-            //    { Testing.Productions.S___TO____a__S___a, },
-            //    { Testing.Productions.S___TO___S_62__S__S_62, },
-            //    { Testing.Productions.S___TO___, empty },
-            //});
-            //var rootNode = parser.Parse(content).ToList();
-
-            //Console.WriteLine($"{rootNode}");
+            var content = @"a          b";
+            var root = Testing.Cst.Parse(content);
+            Console.WriteLine($"{root}");
         }
     }
 }
